532847d6afdf24629593e0b79b16f5c0debe88ad
Sandra Lourenco <sandralourenc@yahoo.com>
2022-04-09 17:53:21 +0100

    Tests update

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -157,7 +157,7 @@ describe("Central de Atendimento ao Cliente TAT", function () {
     cy.get('#privacy a').should('have.attr','target', '_blank')
   });
 
-  it('acessa a página da política de privacidade removendo o target e então clicanco no link', function() {
+  it('acessa a página da política de privacidade removendo o target e então clicando no link', function() {
      cy.get('#privacy a')
      .invoke('removeAttr', 'target')
      .click()
